9be0a91df05192f3bc3d36f04e195d0c3290e530
Krishna0126 <chivukula.krishnaphani@gmail.com>
2021-11-07 22:22:22 +0530

    2commit

---
+++ forEach.js
@@ -0,0 +1,10 @@
+  var objs = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+
+objs.forEach (function (obj) 
+{
+    console.log(obj);
+});
